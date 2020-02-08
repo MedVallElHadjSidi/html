d870d1f4fe82d6f012e60516d7eb1693a9ded63e
MedVall <59884964+MedVallElHadjSidi@users.noreply.github.com>
2020-02-08 16:12:02 +0100

    myfirst

---
+++ scri.js
@@ -0,0 +1,21 @@
+
+function me() {
+   // body...
+  {
+  if(getComputedStyle(d1).display != "none"){
+    d1.style.display = "none";
+  } else {
+    d1.style.display = "block";
+  }
+}
+}
+
+function togg(){
+  if(getComputedStyle(d2).display != "none"){
+    d2.style.display = "none";
+  } else {
+    d2.style.display = "block";
+  }
+};
+
+MvLinked_3343
